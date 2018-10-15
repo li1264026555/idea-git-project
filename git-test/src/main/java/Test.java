99a702a0f3f1bef9b18567cb1527e99f71da4a1a
lizhikui <smiling_li@aliyun.com>
2018-10-15 20:26:32 +0800

    添加了两个测试类

---
+++ git-test/src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @program: idea-git-project
+ * @description: 测试一下
+ * @author: smiling_li
+ * @date: 2018-10-15 20:25
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git测试,,");
+    }
+}
---
+++ git-test/src/test/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @program: idea-git-project
+ * @description: 测试类
+ * @author: smiling_li
+ * @date: 2018-10-15 20:22
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,world..");
+    }
+}
